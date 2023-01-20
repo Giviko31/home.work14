1dc8567a742d1c913338214cf35cce3a457efc4b
Giviko31 <givigivishvili209@gmail.com>
2023-01-20 10:40:50 +0400

    wrote my homework 14

---
+++ script.js
@@ -0,0 +1,54 @@
+///////1//////
+// person = {
+//     fullName: "givi",
+//     lastName: "givishvili",
+//     age: 26,
+// }
+
+// console.log(person.fullName)
+
+// /___________________
+
+
+////////2/////////
+
+// let age = 26;
+// let fullName = "Givi";
+// let address = "vashlijvari"
+// let hello = `My name is ${fullName} and im living in ${address} and I am ${age} years old`
+// console.log(hello);
+
+
+//______________________
+
+
+
+/////////3/////////
+
+// let age = 26;
+
+// if (age <= 26) {
+//     console.log ("I am a teenager");
+// } else { 
+//     console.log ("I am an adult")
+// }
+
+//_________________________
+
+
+
+///////////4///////////
+
+// როცა გვსურს ერთმანეთის მიმდევრობით გამოიტანოს რიცხვები 
+
+// for (let i = 1; i <=5; i++) {
+//     console.log(i);
+// }
+
+// როცა გვსურს (x = i+=4) გამოტოვებით გამოვიტანოთ ციფრები 
+
+// for (let i = 1; i <= 21; i+=4) {
+//     console.log(i);
+// }
+
+//_________________________
